54c101baca23be7991fd129ca683b27794c7ae8a
Tom de Bruin <tom@deadlyhifi.com>
2016-04-19 09:14:48 +0100

    Manually create salts

--- config/wp-salts.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('AUTH_KEY', 'd|M^/9IpStacJ>q~YWmMu3W,b39|OHeRHRsEn0U5-V7pJ]e,HrG1_>V Sq_=Y}K+');
-define('SECURE_AUTH_KEY', 'D3wRq*i:y&l80q1,I.Vp c/As(t*539.t+LK8UG,wS1[9p&djxs+zqsS.LvZb`Uv');
-define('LOGGED_IN_KEY', ' =5RN?K9Q/TmnvpwTIo(YGxS(Te-KZbtS6!+4h1hfgtsCJP_*30B:[-%D{V$KW,~');
-define('NONCE_KEY', 'p3--!I>&Oh.&RJYu|kV;,B!xmr:X%~M~Tt9q1Y=c5<)iwDDwN]rHm>4yg1 -Ru!:');
-define('AUTH_SALT', 'OFnb~fdV, 4qm.MZ(4=`TWpZj?F0tbX7H!8x&#)!VK}7Eysj~l|I8p,*nzEGA>a8');
-define('SECURE_AUTH_SALT', 'Z8=CH@?>i]WMj%V?}VGU@Ay]Z2r}RrUc}MEukCisn54wFmus70#E}:pMZHycZ(fM');
-define('LOGGED_IN_SALT', 'rJ6B#^T{Gkh#wB`~>2u^ZJnk*LnCqs%H2G8AholOytmUVcKJ55lqbyB(!Ph<7_uv');
-define('NONCE_SALT', 'o~Wwd8!C< [I]u(Q{~7}o.4{J@#dT=z8  `}Q0oEJ/#trR!cHD~VxUH7sf@@?:F_');
--- public/wp-config.php
+++ public/wp-config.php
@@ -7,3 +7,13 @@
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 require_once(dirname(__DIR__) . '/config/wp-application.php');
 require_once(ABSPATH . 'wp-settings.php');
+// Salts ///////////////////////////////////////////////////////////////////////
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
+
